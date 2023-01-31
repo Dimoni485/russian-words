561548494925e7db3a572f40b715163db9d9ba07
Dmitry Shestoukh <dimoni485@bk.ru>
2023-01-31 13:43:40 +0300

    add main

---
+++ main.py
@@ -0,0 +1,22 @@
+
+
+not_world = list(''.upper())
+yes_world = list(''.upper())
+
+with open('summary.txt') as rus_fl:
+    rus_world_db = rus_fl.readlines()
+
+for rus_word in rus_world_db:
+    x = 0
+    rus_word_strip = rus_word.strip('\n').upper()
+    if len(rus_word_strip) == 5:
+        for y_key in yes_world:
+            if y_key not in rus_word_strip:
+                x = 1
+
+        for n_key in not_world:
+            if n_key in rus_word_strip:
+                x = 1
+        if x == 0:
+            print(f'{rus_word_strip}')
+            break
